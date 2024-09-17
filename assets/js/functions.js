c2fd810cf2b6cc02c6069e5bec06354ea1a60ce2
Suchanyecz Alex <suhalex04@gmail.com>
2024-09-17 10:03:45 +0200

    themeSwitchFix

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,28 +1,26 @@
 // Get the toggle switch element
-const darkModeToggle = document.getElementById('themeToggle');
+const lightModeToggle = document.getElementById('themeToggle');
 
 // Check if the user has a previously saved preference
 const currentTheme = localStorage.getItem('theme');
 if (currentTheme) {
     document.body.classList.add(currentTheme);
-    if (currentTheme === 'dark-mode') {
-        darkModeToggle.checked = true;
+    if (currentTheme === 'light-mode') {
+        lightModeToggle.checked = true;
     }
 }
 
 // Event listener for the toggle switch
-darkModeToggle.addEventListener('change', function() {
+lightModeToggle.addEventListener('change', function() {
 
-    //When checked, it is DARK MODE
+    //When checked, it is LIGHT MODE
     if (this.checked) {
-        document.body.classList.remove('light-mode');
-        document.body.classList.add('dark-mode');
-        localStorage.setItem('theme', 'dark-mode'); // Save user preference
-    } else {
-        
-    //When unchecked, it is LIGHT MODE
-        document.body.classList.remove('dark-mode');
         document.body.classList.add('light-mode');
         localStorage.setItem('theme', 'light-mode'); // Save user preference
+    } else {
+        
+    //When unchecked, it is DARK MODE 
+        document.body.classList.remove('light-mode');
+        localStorage.setItem('theme', 'dark-mode'); // Save user preference
     }
 });
